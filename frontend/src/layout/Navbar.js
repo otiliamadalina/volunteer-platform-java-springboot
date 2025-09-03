0e7ba4bbbde18d277027a8a41445d33e89e7328c
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-09-03 17:42:38 +0300

    added navbar layout

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -1,33 +1,63 @@
 import React from "react";
-/* eslint-disable */
 import {Link} from "react-router-dom";
-
+import "../styles/Navbar.css";
 
 export default function Navbar() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
-                        Volunteer Sphere
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
+            <div className="container-fluid">
 
-                    <Link className="btn btn-outline-light" to="/adduser">
-                        Add User
-                    </Link>
+                {/* Logo */}
+                <Link className="navbar-brand d-flex align-items-center" to="/">
+                    <img src="/assets/volunteer-pink-transparent-logo.png"
+                         alt="Logo"
+                         width="110"
+                         height="50"
+                         className="navbar-logo me-2"
+                    />
+                </Link>
+
+                {/* Toggler pentru mobil */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Link-uri din dreapta */}
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+
+                        <li className="nav-item">
+                            <Link className="nav-link custom-link" to="/about">About</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link className="nav-link custom-link" to="/contact">Contact</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link className="nav-link custom-link" to="/events">Events</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link className="btn nav-btn-register me-2" to="/register">Register</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link className="btn nav-btn-login" to="/login">Login</Link>
+                        </li>
+
+
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }
