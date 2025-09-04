9f9abeadf29676efabe5b0ee8b1027d8a069803a
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-09-04 23:16:06 +0300

    added image for footer

--- frontend/src/layout/footer.js
+++ frontend/src/layout/footer.js
@@ -6,10 +6,16 @@ import "../styles/footer.css"
 
 export default function Footer() {
     return (
+<>
+        <div className="footer-image-wrapper">
+            <img src="/assets/multicolour-hands.jpg" alt="Footer Banner" className="footer-image" />
+        </div>
+
         <footer className="footer bg-custom text-light py-4">
             <div className="container-fluid d-flex justify-content-between align-items-center">
                 <span className="footer-text">&copy; {new Date().getFullYear()} Volunteer Sphere</span>
             </div>
         </footer>
+</>
     );
 }
