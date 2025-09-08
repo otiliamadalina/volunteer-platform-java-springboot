55bd1fb055b5a1a7fec046c4538af88e278e23b8
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-09-08 20:29:33 +0300

    feat: add ProtectedRoute component and secure dashboard access by role

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,9 @@ import ManageOrganisations from "./pages/admin/manageOrganisations";
 import ManageEvents from "./pages/admin/manageEvents";
 import ManageActivity from "./pages/admin/manageActivity";
 import ManageFeedbacks from "./pages/admin/manageFeedbacks";
+import ProtectedRoute from "./components/protectedRoute";
+import VolunteerDashboard from "./pages/volunteer/volunteerDashboard";
+import OrganisationDashboard from "./pages/org/organisationDashboard";
 
 function App() {
     return (
@@ -28,20 +31,47 @@ function App() {
                     {/* aici adaug toate butoanele care duc catre alte pagini ABSOLUT TOATE*/}
                     <Route exact path="/" element={<Home/>}/> {/* cand url ul este / se afiseaza componenta Home */}
 
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/volunteerOrOrg" element={<VolunteerOrOrg />} />
-                    <Route path="/registerAsVolunteer" element={<RegisterAsVolunteer />} />
-                    <Route path="/registerAsOrg" element={<RegisterAsOrg />} />
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/volunteerOrOrg" element={<VolunteerOrOrg/>}/>
+                    <Route path="/registerAsVolunteer" element={<RegisterAsVolunteer/>}/>
+                    <Route path="/registerAsOrg" element={<RegisterAsOrg/>}/>
 
                     {/*<Route path="/volunteerDashboard" element={<VolunteerDashboard />} />*/}
                     {/*<Route path="/organisationDashboard" element={<OrganisationDashboard />} />*/}
 
-                    <Route path="/adminDashboard" element={<AdminDashboard />} />
-                    <Route path="/adminDashboard/manageVolunteers" element={<ManageVolunteers />} />
-                    <Route path="/adminDashboard/manageOrganisations" element={<ManageOrganisations />} />
-                    <Route path="/adminDashboard/manageEvents" element={<ManageEvents />} />
-                    <Route path="/adminDashboard/manageActivity" element={<ManageActivity />} />
-                    <Route path="/adminDashboard/manageFeedbacks" element={<ManageFeedbacks />} />
+                    {/*<Route path="/adminDashboard" element={<AdminDashboard/>}/>*/}
+                    <Route path="/adminDashboard/manageVolunteers" element={<ManageVolunteers/>}/>
+                    <Route path="/adminDashboard/manageOrganisations" element={<ManageOrganisations/>}/>
+                    <Route path="/adminDashboard/manageEvents" element={<ManageEvents/>}/>
+                    <Route path="/adminDashboard/manageActivity" element={<ManageActivity/>}/>
+                    <Route path="/adminDashboard/manageFeedbacks" element={<ManageFeedbacks/>}/>
+
+                    <Route
+                        path="/adminDashboard"
+                        element={
+                            <ProtectedRoute allowedRole="ADMIN">
+                                <AdminDashboard/>
+                            </ProtectedRoute>
+                        }
+                    />
+
+                    <Route
+                        path="/volunteerDashboard"
+                        element={
+                            <ProtectedRoute allowedRole="VOLUNTEER">
+                                <VolunteerDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+
+                    <Route
+                        path="/organisationDashboard"
+                        element={
+                            <ProtectedRoute allowedRole="ORGANISATION">
+                                <OrganisationDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
 
 
                 </Routes>
